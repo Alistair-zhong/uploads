2c2be1145775a5ab1605ff66475d1a759a2b241d
niro <48372r@gmail.com>
2020-05-08 21:58:03 -0400

    修改隐私配置，解决bug

--- config/uploads.php
+++ config/uploads.php
@@ -11,13 +11,6 @@ return [
     /**
      * 此处是配置config/filesystem文件中的disks中的存储磁盘
      * 最终‘default’和‘default_disk’的配置会合并到config/filesystem文件中
-     * 格式为 'google'    => [
-     *   "driver"        => 'google',
-     *   "clientId"      => '1066414826686-c2vfuorn7pao821j14ef9hl4lfluhc8n.apps.googleusercontent.com',
-     *   "clientSecret"  => 'NSZCwTyV9-zIRwQG-Enj1UdM',
-     *   "refreshToken"  => '1//    04TdDrqC8nOToCgYIARAAGAQSNwF-L9IrYKGDP7xINoOZ_XRaOMUIefcxIW4sYKCgXlyxqJa-nyTyAO4z3t6NrS57yikAEG3qjTQ',
-     *   "folderId"      => '1NrzQCTiZsRZdj1sAyARDFjd7tTaFjP05',
-     * ],
      */
     'default_disk'    => [
         "driver"        => '',
